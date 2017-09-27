9c63fb747de8c22fd52d81de64099de29b5a6305
frankwym <1085595193@qq.com>
2017-09-27 20:27:17 +0800

    first commit

---
+++ src/com/bean/MapFormat.java
@@ -0,0 +1,36 @@
+package com.bean;
+
+import java.util.Date;
+
+public class MapFormat {
+	private String FORMATNAME;
+	private String username;
+	private Date time;
+	private String content;
+	public String getFORMATNAME() {
+		return FORMATNAME;
+	}
+	public void setFORMATNAME(String FORMATNAME) {
+		this.FORMATNAME = FORMATNAME;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+}
